ac8b3d1d3fd22ea539a7b94a9b1ad58688c356bf
ASZhartun <as.zhartun@gmail.com>
2022-12-05 15:55:28 +0200

    fix: merged filefolderpicker with base project

--- src/com/example/sparknotes/FilePickerActivity.java
+++ src/com/example/sparknotes/FilePickerActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 Aeesh Joseph
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.sparknotes;
 
@@ -22,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 import com.coderplus.filepicker.R;
 
@@ -95,11 +81,11 @@ public class FilePickerActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.list_holder);
+		setContentView(R.layout.activity_file_picker_list_holder);
 		// Set the view to be shown if the list is empty
 		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		LayoutInflater inflator = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View emptyView = inflator.inflate(R.layout.file_picker_empty_view, null);
+		View emptyView = inflator.inflate(R.layout.activity_file_picker_empty_view, null);
 		((ViewGroup) getListView().getParent()).addView(emptyView);
 		getListView().setEmptyView(emptyView);
 
@@ -135,7 +121,7 @@ public class FilePickerActivity extends ListActivity {
 		if (getIntent().getBooleanExtra(EXTRA_SELECT_DIRECTORIES_ONLY, false))
 			pickType = 2;
 
-		Button ok = (Button) findViewById(R.id.ok);
+		Button ok = (Button) findViewById(R.id.activity_file_picker_ok);
 		ok.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				returnResults();
@@ -256,7 +242,7 @@ public class FilePickerActivity extends ListActivity {
 		private ArrayList<File> checkedObjects = new ArrayList<File>();
 
 		public FilePickerListAdapter(Context context, List<File> objects) {
-			super(context, R.layout.file_picker_list_item, android.R.id.text1, objects);
+			super(context, R.layout.activity_file_picker_list_item, android.R.id.text1, objects);
 			mObjects = objects;
 		}
 
@@ -283,7 +269,7 @@ public class FilePickerActivity extends ListActivity {
 			if (convertView == null) {
 				LayoutInflater inflater = (LayoutInflater) getContext()
 						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-				row = inflater.inflate(R.layout.file_picker_list_item, parent, false);
+				row = inflater.inflate(R.layout.activity_file_picker_list_item, parent, false);
 			} else {
 				row = convertView;
 			}
